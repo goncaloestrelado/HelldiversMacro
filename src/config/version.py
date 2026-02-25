49f3f733367a53eb896689421e5a494148a9da24
goncaloestrelado <goncalo.estrelado@hotmail.com>
2026-02-25 18:52:12 +0000

    fix(version): update APP_NAME to reflect new branding

--- src/config/version.py
+++ src/config/version.py
@@ -3,6 +3,6 @@ Version information for Helldivers Numpad Macros
 """
 
 VERSION = "beta0.3.0"
-APP_NAME = "Helldivers Numpad Macros"
+APP_NAME = "Helldivers 2 - Strat Commander"
 GITHUB_REPO_OWNER = "goncaloestrelado"
 GITHUB_REPO_NAME = "HelldiversMacro"
