074c6e882b31e07b2b202cb76b3dc4604e210b20
Gonçalo Estrelado <93692940+goncaloestrelado@users.noreply.github.com>
2026-03-23 05:54:36 +0000

    0.3.3 - added stratagems from Entrenched Division warbond

--- src/config/version.py
+++ src/config/version.py
@@ -2,7 +2,7 @@
 Version information for Helldivers Numpad Macros
 """
 
-VERSION = "beta0.3.2"
+VERSION = "beta0.3.3"
 APP_NAME = "Helldivers 2 - Strat Commander"
 GITHUB_REPO_OWNER = "goncaloestrelado"
 GITHUB_REPO_NAME = "HelldiversMacro"
