f719adb699238e14c5416ad415d3fc6d1fe787a0
Gonçalo Estrelado <93692940+goncaloestrelado@users.noreply.github.com>
2026-04-24 23:28:45 +0100

    chore: update version to 0.4.0 in manifest, installer, and version files

--- src/config/version.py
+++ src/config/version.py
@@ -2,7 +2,7 @@
 Version information for Helldivers Numpad Macros
 """
 
-VERSION = "beta0.3.3"
+VERSION = "0.4.0"
 APP_NAME = "Helldivers 2 - Strat Commander"
 GITHUB_REPO_OWNER = "goncaloestrelado"
 GITHUB_REPO_NAME = "HelldiversMacro"
